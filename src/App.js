8326c7dd34914ca498ad3e970739718b548c109b
rinel <rineliniguezsosa@gmail.com>
2023-02-26 18:48:18 -0600

    importando el componente Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes,Route } from 'react-router-dom'
 import { About } from './01-configuring routes/components/About';
 import { Home } from './01-configuring routes/components/Home';
-
+import { Navbar } from './02-Links/Navbar';
 
 function App() {
   return (
