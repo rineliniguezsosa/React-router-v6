3bbeb44ad7ba442209777ed1e1cfccb5192ee558
rinel <rineliniguezsosa@gmail.com>
2023-02-26 19:04:38 -0600

    cambiando el componente Link por NavLink

--- src/02-Links/Navbar.js
+++ src/02-Links/Navbar.js
@@ -14,7 +14,7 @@ export const Navbar = () => {
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
+                <NavLink className="nav-link" to="/about">About</NavLink>
                 </li>
             </ul>
             </div>
