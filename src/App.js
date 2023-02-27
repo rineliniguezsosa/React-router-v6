3f169c0004ba4b3f4111277798d141d170444553
rinel <rineliniguezsosa@gmail.com>
2023-02-26 18:06:47 -0600

    agregando el componente Route donde la ruta en el navegador sera /about y renderizara el componente About

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
     </Routes>
   );
 }
