ad5d453e90ab336fd00f9f713f3b840d62082e19
rinel <rineliniguezsosa@gmail.com>
2023-02-26 18:02:15 -0600

    importando el componente Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Routes,Route } from 'react-router-dom'
+import { Home } from './01-configuring routes/components/Home';
+
+
 
 function App() {
   return (
     <Routes>
-     
+      
     </Routes>
   );
 }
