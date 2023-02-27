2b76b54ef28ff6234c93fbc833ae29af6de17cce
rinel <rineliniguezsosa@gmail.com>
2023-02-26 19:22:56 -0600

    agregando el componente Navlink y verificando si la url esta activa o no

--- src/02-Links/Navbar.js
+++ src/02-Links/Navbar.js
@@ -14,7 +14,7 @@ export const Navbar = () => {
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                <NavLink className="nav-link" to="/about">About</NavLink>
+                <NavLink className={({isActive})=> `${(isActive)} ? nav-link : ''`} to="/about">About</NavLink>
                 </li>
             </ul>
             </div>
