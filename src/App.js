1b736ee7edcc11303a8ce2c1f20dc4ffaf0b7f7c
rinel <rineliniguezsosa@gmail.com>
2023-02-26 18:03:01 -0600

    importando el componente About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Routes,Route } from 'react-router-dom'
+import { About } from './01-configuring routes/components/About';
 import { Home } from './01-configuring routes/components/Home';
-
 
 
 function App() {
   return (
     <Routes>
-      
+
     </Routes>
   );
 }
