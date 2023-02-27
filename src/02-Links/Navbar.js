5135f59c50e4728596b8fa04d6974945704cdaca
rinel <rineliniguezsosa@gmail.com>
2023-02-26 18:46:01 -0600

    cambiando los elementos a por componentes Link

--- src/02-Links/Navbar.js
+++ src/02-Links/Navbar.js
@@ -11,10 +11,10 @@ export const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
                 <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+                <Link className="nav-link" to="/about">About</Link>
                 </li>
             </ul>
             </div>
