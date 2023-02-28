1d82c2023db30b39fa8334cb75d648144d725931
rinel <rineliniguezsosa@gmail.com>
2023-02-27 20:58:21 -0600

    creando la ruta Orden

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/about" element={<About></About>}></Route>
+      <Route path="/orden" element={<Orden></Orden>}></Route>
     </Routes>
     </>
   );
