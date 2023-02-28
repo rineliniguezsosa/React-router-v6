a9829d53074e705cb9e6c4b077818a6bb28561f2
rinel <rineliniguezsosa@gmail.com>
2023-02-27 20:57:28 -0600

    importando el componente Orden

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes,Route } from 'react-router-dom'
 import { About } from './01-configuring routes/components/About';
 import { Home } from './01-configuring routes/components/Home';
 import { Navbar } from './02-Links/Navbar';
+import { Orden } from './03-navigating programmatically/Orden';
+
 
 function App() {
   return (
