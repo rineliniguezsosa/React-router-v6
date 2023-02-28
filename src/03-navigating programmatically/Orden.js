bfe30c4e9ca558aa7648fe5fab6a2c94a781e2d2
rinel <rineliniguezsosa@gmail.com>
2023-02-27 21:32:52 -0600

    creando el elemento button para navegar hacia la ruta principal

--- src/03-navigating programmatically/Orden.js
+++ src/03-navigating programmatically/Orden.js
@@ -4,6 +4,8 @@ export const Orden = () => {
   return (
     <>
     <span>Orden realizada</span>
+    <br></br>
+    <button onClick={()=> navigate('/',{replace:true})} className="btn btn-primary">regresar</button>
     </>
   )
 }
