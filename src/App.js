85e6cfbdd34c1283129b8e98966486a2fd776b6d
rinel <rineliniguezsosa@gmail.com>
2023-03-06 21:54:42 -0600

    importando el componente Detallesusuario

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { NoMatch } from './04-No Match Route/NoMatch';
 import { Caracteristicas } from './05-Nested routes/Caracteristicas';
 import { Nuevo } from './05-Nested routes/Nuevo';
 import { Products } from './05-Nested routes/Products';
+import { Detallesusuario } from './06-Dinamic routes/Detallesusuario';
 import { Usuarios } from './06-Dinamic routes/Usuarios';
 
 
