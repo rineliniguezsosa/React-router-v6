eb5451800ed1426e8eed5d70285c97b0abe0aec5
rinel <rineliniguezsosa@gmail.com>
2023-03-10 23:04:04 -0600

    comentando la importacion del componente About

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes,Route } from 'react-router-dom'
-import { About } from './01-configuring routes/components/About';
+// import { About } from './01-configuring routes/components/About';
 import { Home } from './01-configuring routes/components/Home';
 import { Navbar } from './02-Links/Navbar';
 import { Orden } from './03-navigating programmatically/Orden';
